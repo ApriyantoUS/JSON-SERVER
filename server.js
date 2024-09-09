26fdbbf397be06e7cadcc8998083b7eee88c6e70
Apriyanto Ulami Setiawan <apriyantoa252@gmail.com>
2024-09-09 07:26:30 +0700

    "update"

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // Ganti dengan nama file database Anda
+const router = jsonServer.router("db.json"); // Ganti dengan nama file database Anda
 const middlewares = jsonServer.defaults();
 
 // Tambahkan middleware kustom atau logika lainnya di sini
 server.use(middlewares);
 
 // Contoh route kustom
-server.get('/custom-route', (req, res) => {
-  res.jsonp({ message: 'This is a custom route!' });
+server.get("/custom-route", (req, res) => {
+  res.jsonp({ message: "This is a custom route!" });
 });
 
 server.use(router);
